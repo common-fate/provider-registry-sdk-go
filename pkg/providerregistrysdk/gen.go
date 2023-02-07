4bdeb24a75ff00277a13cf33c43d61d79d1738c9
meyerjrr <jrmeyer339@gmail.com>
2023-02-07 13:17:56 +1100

    added mock client for pr sdk

---
+++ pkg/providerregistrysdk/gen.go
@@ -0,0 +1,3 @@
+package providerregistrysdk
+
+//go:generate go run github.com/golang/mock/mockgen -destination=prmocks/COMMONFATE_MOCK_PROVIDER_REGISTRY_client.go -package=prmocks . ClientWithResponsesInterface
