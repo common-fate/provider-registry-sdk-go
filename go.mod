33ec8e8aebbefde05748ef7ea2ddf3f55f3afb67
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-03-08 12:37:24 +0000

    add registry client wrapper which improves error handling
    no need to individually check response codes with this approach

--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.17.5
 	github.com/aws/aws-sdk-go-v2/service/lambda v1.30.0
 	github.com/common-fate/apikit v0.2.0
+	github.com/common-fate/useragent v0.1.0
 	github.com/deepmap/oapi-codegen v1.11.0
 	github.com/getkin/kin-openapi v0.107.0
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/golang/mock v1.6.0
+	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.1
 )
 
@@ -46,7 +48,7 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.21.0 // indirect
+	go.uber.org/zap v1.21.0
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
---
+++ pkg/registryclient/client.go
@@ -0,0 +1,91 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/common-fate/provider-registry-sdk-go/pkg/providerregistrysdk"
+	"github.com/common-fate/useragent"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+type Doer interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+// ErrorHandlingClient checks the response status code
+// and creates an error if the API returns greater than 300.
+type ErrorHandlingClient struct {
+	Client Doer
+	Logger *zap.SugaredLogger
+}
+
+func (rd *ErrorHandlingClient) Do(req *http.Request) (*http.Response, error) {
+	// add a user agent to the request
+	ua := useragent.FromContext(req.Context())
+	if ua != "" {
+		req.Header.Add("User-Agent", ua)
+	}
+
+	res, err := rd.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode < 300 {
+		// response is ok
+		return res, nil
+	}
+
+	// if we get here, the API has returned an error
+	// surface this as a Go error so we don't need to handle it everywhere in our CLI codebase.
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return res, errors.Wrap(err, "reading error response body")
+	}
+
+	// if we get here, the API has returned a response code > 300.
+	// we treat this as an error.
+	return res, fmt.Errorf("the Provider Registry returned an error (code %v): %s", res.StatusCode, string(body))
+}
+
+type ClientOpts struct {
+	APIURL     string
+	HTTPClient Doer
+}
+
+// WithAPIURL overrides the API URL.
+// If the url is empty, it is not overriden and the regular
+// API URL from aws-exports.json is used instead.
+//
+// This can be used for local development to provider a localhost URL.
+func WithAPIURL(url string) func(co *ClientOpts) {
+	return func(co *ClientOpts) {
+		co.APIURL = url
+	}
+}
+
+// New creates a new client, specifying the URL and context directly.
+// The client loads the OAuth2.0 tokens from the system keychain.
+// The client automatically refreshes the access token if it is expired.
+func New(ctx context.Context, server, context string, opts ...func(co *ClientOpts)) (*Client, error) {
+	co := &ClientOpts{
+		APIURL:     "https://api.registry.commonfate.io",
+		HTTPClient: http.DefaultClient,
+	}
+
+	for _, o := range opts {
+		o(co)
+	}
+
+	httpClient := &ErrorHandlingClient{Client: co.HTTPClient}
+
+	return providerregistrysdk.NewClientWithResponses(server, providerregistrysdk.WithHTTPClient(httpClient))
+}
+
+// Client is an alias for the exported Go SDK client type
+type Client = providerregistrysdk.ClientWithResponses
